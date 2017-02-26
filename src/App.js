6add099ce0ab8ece71b52e4293b32ecc4fc3b00e
Tokyo <ahmed.tokyo1@gmail.com>
2017-02-26 05:56:56 +0200

    enhance app code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-// import './App.css';
-//
+import './App.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" width="100px" height="100px" alt="logo" />
-          <h2>Rurubot</h2>
         </header>
         <main className="App-body">
           <h2>Rurubot</h2>
         </main>
         <footer className="App-footer">
-          <p>Copyright (c) 2017 Dreidev All Rights Reserved.</p>
+          <p>Copyright (c) 2017 <a href="http://dreidev.com">Dreidev</a> All Rights Reserved.</p>
         </footer>
       </div>
     );
